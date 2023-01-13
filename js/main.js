f820895ba1b66987d82d1bac5d3b808cbd2ceb7f
Valentin Cheny <92431878+Zeval5466@users.noreply.github.com>
2023-01-13 15:27:07 +0100

    Création du dossier Js
    Test Js
    ajout de .gitignore

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { Farmbot } from "farmbot";
+
+var SUPER_SECRET_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ1bmtub3duIiwic3ViIjoxOTkyOSwiaWF0IjoxNjczNTMxNDQ2LCJqdGkiOiIzMTMwMzRmYS1mMWExLTQzMDktYTcxNy0zZTQyZjA3ZjkzZDMiLCJpc3MiOiIvL215LmZhcm0uYm90OjQ0MyIsImV4cCI6MTY3ODcxNTQ0NiwibXF0dCI6ImNsZXZlci1vY3RvcHVzLnJtcS5jbG91ZGFtcXAuY29tIiwiYm90IjoiZGV2aWNlXzE5OTYzIiwidmhvc3QiOiJ4aWNvbmZ1bSIsIm1xdHRfd3MiOiJ3c3M6Ly9jbGV2ZXItb2N0b3B1cy5ybXEuY2xvdWRhbXFwLmNvbTo0NDMvd3MvbXF0dCJ9.u_jBXWLmhatHfXz2yd-fWUa6IHng2ocQnuJ6ZpnA1gsKxYr1Gy5GTdFFdM6jFrSGcSrXktp2Jp_t93L2IM0461mXmpvxPViVRrbTKDEubI-g0jmLxBvOx84NDVlN-Oees0hXvOxj_ozLTY6_krYOlrs5UncqhBKDsEn9SHGojXdDek9TtwDd9mpdwieSNEe_JQa-knLigrHyk-tb0yQA1yz-jedE1bycnPNTXTa_qIHgDbMU7bO1VkhGiYcB3wbTioG3NG41oFBVP9qLBist1fu0JtNhMOERlwHQM_189Mp3OO85ilcHxpjb-i9rtMuZjZy99ca8IYEVfqhjip3p0w";
+let bot = new Farmbot({ token: SUPER_SECRET_TOKEN });
+
+bot
+    .connect()
+    .then(function () {
+        return bot.moveRelative({ x: 100, y: 2, z: 3, speed: 100 });
+    });
